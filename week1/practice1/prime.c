62a66dc71eeb9ea2317b09859f77dbe96bd193b3
Margaret <144652524+margaret-ach@users.noreply.github.com>
2023-10-25 10:05:13 -0400

    Create prime.c

---
+++ week1/practice1/prime.c
@@ -0,0 +1,24 @@
+/*
+Background:
+Prime numbers are defined as whole numbers greater than 1, whose only factors 
+are 1 and itself. So 3 is prime because its only factors are 1 and 3, while 4
+is composite and not prime, because it is the product of 2 × 2. In this lab 
+you will write an algorithm to generate all prime numbers in a range specified
+by the user.
+*/
+
+#include <stdio.h>
+#include <stdbool.h>
+
+// Loops through the prime numbers between a minimum and maximum value.
+int prime(int min, int max);
+
+int main()
+{
+    prime(1, 10);
+}
+
+int prime(int min, int max)
+{
+
+}
