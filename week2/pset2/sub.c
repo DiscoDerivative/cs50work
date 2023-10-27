d976ba37335ded3a6ac52b0a9345243299be287d
Margaret <margaret.ach7@gmail.com>
2023-10-27 16:15:41 -0400

    added sub problem set from week 2

---
+++ week2/pset2/sub.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+
+    // Throw error if user doesn't enter a key or adds to many arguments
+    if (argc != 2)
+    {
+        printf("Usage: ./substitution key");
+        return 1;
+    }
+    
+}
